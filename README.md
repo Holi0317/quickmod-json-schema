635468025ed90706532575c6a41ac772bf809cfb
holi0317 <holliswuhollis@gmail.com>
2015-06-27 14:14:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# quickmod-json-schema
+Json schema for quickmod
+
+## Introduction
+A QuickMod file is a JSON file that contains metadata about a mod for the sandbox game Minecraft. This metadata can be things like name, description, authors, different URLs (website, issue tracker, donations etc.), but also machine readable information about how to install the mod.
+
+(Copied from [Quickmod website](http://blog.02jandal.xyz/QuickModDoc/index.html))
+
+Json schema is a schema that is used for validating the structure of JSON data.
+
+This repo combines this two things together and make QuickMod development easier.
+
+## Usage
+Different languages have their own implementation of json schema validation. Google your preferred language with 'json schema' will do.
+
+## Files
+A JSON does not support comments, here are the files representing.
+
+index.json -- According to [Spec](http://blog.02jandal.xyz/QuickModDoc/qm_index_spec.html), the index quickmod file
+main.json -- Main quickmod file. Including version and such
---
+++ tests/test_main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Test if index.json will validate correct json file.
+'''
+
+import unittest
+import json
+import os
+
+from jsonschema import validate, Draft4Validator
+from jsonschema.exceptions import ValidationError, SchemaError
+
+from . import __file__ as test_directory
+
+
+THIS_DIR = os.path.dirname(test_directory)
+
+
+def json_load(file_path):
+    'Open, load and close json file wrapper'
+    with open(file_path, 'r') as file:
+        content = json.loads(file.read())
+    return content
+
+
+class TestIndexJson(unittest.TestCase):
+    'Tests for index quickmod file'
+    def __init__(self, *args, **kwargs):
+        'Setup variables'
+        super(TestIndexJson, self).__init__(*args, **kwargs)
+
+        data_dir = os.path.join(THIS_DIR, 'data/index')
+
+        # Paths of files
+        schema = os.path.join(THIS_DIR, '../index.json')
+
+        # Without optional data
+        valid_data = os.path.join(data_dir, 'valid1.json')
+        # With optional data, baseUrl
+        valid_data_optional = os.path.join(data_dir, 'valid2.json')
+
+        # Some items got missing url or uid key
+        invalid_data = os.path.join(data_dir, 'invalid1.json')
+        # Empty index array
+        invalid_data_empty_index = os.path.join(data_dir, 'invalid2.json')
+
+        self.schema = json_load(schema)
+        self.valid_datas = [json_load(valid_data),
+                            json_load(valid_data_optional)]
+        self.invalid_datas = [json_load(invalid_data),
+                              json_load(invalid_data_empty_index)]
+
+    def test_schema_valid(self):
+        'Test if schema is valid'
+        try:
+            Draft4Validator.check_schema(self.schema)
+        except SchemaError as error:
+            self.fail('Index.json got incorrect schema. %s' % error)
+
+    def test_valid(self):
+        'If valid data is passed in, no exception should be raised'
+        # If exception raised, fail this test
+        try:
+            for data in self.valid_datas:
+                validate(data, self.schema)
+        except ValidationError as error:
+            self.fail('Correct qm index data got error. %s' % error)
+
+    def test_incorrect_data(self):
+        'If invalid data is passed in, ValidationError should be raised'
+        with self.assertRaises(ValidationError):
+            for data in self.invalid_datas:
+                validate(data, self.schema)
